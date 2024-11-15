20fabac2294dcc6600170f2a51cb11ca05a43713
Iago Merida <iago.merida@enti.cat>
2024-11-15 11:19:57 +0100

    Ultima clase viernes 15.11

--- client.sh
+++ client.sh
@@ -10,8 +10,30 @@ echo "DMAM" | nc 127.0.0.1 $PORT
 
 DATA=`nc -l $PORT`
 
-if [ $DATA != "OK_HEADER" ]
+if [ "$DATA" != "OK_HEADER" ]
 then
 	echo "Error 1: Cabecera incorrecta"
 	exit 1
 fi
+echo "3.ENVIANDO FILE_NAME dragon.txt"
+echo "FILE_NAME dragon.txt" | nc 127.0.0.1 $PORT
+DATA=`nc -l $PORT`
+if [ "$DATA" != "OK_PREFIJO" ]
+then
+	echo "ERROR 2: Prefijo incorrecto"
+	exit 2
+fi
+
+echo "5. ENVIANDO CONTENIDO"
+CONTENIDO=`cat /client/dragon.txt`
+echo "$CONTENIDO" | nc 127.0.0.1 $PORT
+
+if [ "$DATA" != "OK_FILE_NAME" ]
+then
+	echo "ERROR 2: El nombre que se envió incorrectamente"
+	exit 2
+fi
+
+echo "8. ENVIANDO CONTENIDO"
+
+cat client/dragon.txt | nc 127.0.0.1 $PORT
--- server.sh
+++ server.sh
@@ -17,5 +17,22 @@ then
 fi
 
 echo "2. CHECK OK - Enviando OK _HEADER"
-echo "OK_HEADER" | nc localhost 2022
+echo "OK_HEADER" | nc 127.0.0.1 2022
 DATA=`nc -l $PORT`
+PREFIJO=`echo "$DATA" | cut -d ' ' -f 1`
+
+if [ "$PREFIJO" != "FILE_NAME" ]
+then
+	echo "ERROR 2: Prefijo incorrecto"
+	echo "KO_PREFIJO" | nc 127.0.0.1 $PORT
+	exit 2
+fi
+
+echo "4. CHECK OK - ENVIANDO OK_PREFIJO"
+echo "OK_PREFIJO" | nc 127.0.0.1 $PORT
+
+echo "6. CHECK OK - ENVIANDO OK_FILE_NAME"
+echo "OK_FILE_NAME" | nc 127.0.0.1 $PORT
+DATA=`nc -l $PORT`
+echo "$DATA" > server/dragon.txt
+
