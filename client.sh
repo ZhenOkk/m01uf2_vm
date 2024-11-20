106cc94cd2ea23b7482372c2510ec1f0c6eae4ef
Iago Merida <iago.merida@enti.cat>
2024-11-20 11:29:54 +0100

    Avance clases 20.11

--- client.sh
+++ client.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
+PORT="2022"
+IP_SERVER=$1
 
+if [ "$1" == "" ]
+then
+	echo "Debes indicar la dirección del servidor"
+	echo "Ejemplo:"
+	echo -e "\t$0 127.0.0.1"
+	exit 1
+	fi
+
+IP=`ip a | grep "score global" | xargs | cut -d " " -f 3 | cut -d "/" -f 1`
 PORT="2022"
 
 echo "Cliente de Dragón Magia Abuelita Miedo 2022"
 
 echo "1. ENVÍO DE CABEZERA"
 
-echo "DMAM" | nc 127.0.0.1 $PORT
+echo "DMAM" | nc $IP_SERVER  $PORT
 
 DATA=`nc -l $PORT`
 
@@ -16,7 +27,7 @@ then
 	exit 1
 fi
 echo "3.ENVIANDO FILE_NAME dragon.txt"
-echo "FILE_NAME dragon.txt" | nc 127.0.0.1 $PORT
+echo "FILE_NAME dragon.txt" | nc $IP_SERVER $PORT
 DATA=`nc -l $PORT`
 if [ "$DATA" != "OK_PREFIJO" ]
 then
@@ -26,7 +37,7 @@ fi
 
 echo "5. ENVIANDO CONTENIDO"
 CONTENIDO=`cat /client/dragon.txt`
-echo "$CONTENIDO" | nc 127.0.0.1 $PORT
+echo "$CONTENIDO" | nc $IP_SERVER $PORT
 
 if [ "$DATA" != "OK_FILE_NAME" ]
 then
@@ -36,4 +47,4 @@ fi
 
 echo "8. ENVIANDO CONTENIDO"
 
-cat client/dragon.txt | nc 127.0.0.1 $PORT
+cat client/dragon.txt | nc $IP_SERVER $PORT
--- server.sh
+++ server.sh
@@ -9,31 +9,37 @@ echo "0. ESCUCHAMOS"
 
 DATA=`nc -l $PORT`
 
-if [ "$DATA" != "DMAM" ]
+HEADER=`echo $DATA | cut -d " " -f 1`
+IP=`echo $DATA | cut -d " " -f 2`
+
+if ["$HEADER" != "DMAM" ]
 then
 	echo "ERROR 1: Cabecera incorrecta"
-	echo "KO_HEADER" | nc localhost 2022
+	echo "KO_HEADER" | nc $IP $PORT
 	exit 1
 fi
 
+echo "La IP del cliente es: $IP"
+
 echo "2. CHECK OK - Enviando OK _HEADER"
-echo "OK_HEADER" | nc 127.0.0.1 2022
+echo "OK_HEADER" | nc $IP $PORT
 DATA=`nc -l $PORT`
+
 PREFIJO=`echo "$DATA" | cut -d ' ' -f 1`
 NOMDRAGO=`echo "$DATA" | cut -d ' ' -f 2`
 
 if [ "$PREFIJO" != "FILE_NAME" ]
 then
 	echo "ERROR 2: Prefijo incorrecto"
-	echo "KO_PREFIJO" | nc 127.0.0.1 $PORT
+	echo "KO_PREFIJO" | nc $IP $PORT
 	exit 2
 fi
 
 echo "4. CHECK OK - ENVIANDO OK_PREFIJO"
-echo "OK_PREFIJO" | nc 127.0.0.1 $PORT
+echo "OK_PREFIJO" | nc $IP $PORT
 
 echo "6. CHECK OK - ENVIANDO OK_FILE_NAME"
-echo "OK_FILE_NAME" | nc 127.0.0.1 $PORT
+echo "OK_FILE_NAME" | nc $IP $PORT
 DATA=`nc -l $PORT`
 echo "$DATA" > server/$NOMDRAGO
 
